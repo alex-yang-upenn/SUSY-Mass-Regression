563c50f76f978dbfbd7591de8ad367e164920a74
alex-yang-upenn <chukyang@sas.upenn.edu>
2025-11-10 00:33:34 -0500

    Optionally keep projection head

--- downstream_model.py
+++ downstream_model.py
@@ -135,6 +135,152 @@ class FinetunedNN(tf.keras.Model):
         return cls(**config_copy)
 
 
+class FinetunedNNFull(tf.keras.Model):
+    def __init__(
+        self,
+        model_path,
+        downstream_units,
+        encoder_architecture_json=None,
+        projection_head_architecture_json=None,
+        output_dim=1,
+        trainable_encoder=True,
+        trainable_projection_head=True,
+        load_from_path=True,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self.model_path = model_path
+        self.downstream_units = downstream_units
+        self.encoder_architecture_json = encoder_architecture_json
+        self.projection_head_architecture_json = projection_head_architecture_json
+        self.output_dim = output_dim
+        self.trainable_encoder = trainable_encoder
+        self.trainable_projection_head = trainable_projection_head
+
+        # Only load the full SimCLR model if instantiating fresh model
+        if load_from_path:
+            full_model = tf.keras.models.load_model(
+                model_path,
+                custom_objects={
+                    "SimCLRNTXentLoss": SimCLRNTXentLoss,
+                    "GraphEmbeddings": GraphEmbeddings,
+                },
+                compile=False,
+            )
+
+            # Extract encoder and projection head from the full model
+            self.encoder = full_model.encoder
+            self.projection_head = full_model.projection_head
+
+            # Set trainability
+            self.set_encoder_trainable(self.trainable_encoder)
+            self.set_projection_head_trainable(self.trainable_projection_head)
+
+            # Store the architectures as JSON strings
+            self.encoder_architecture_json = self.encoder.to_json()
+            self.projection_head_architecture_json = self.projection_head.to_json()
+        else:
+            # Load from stored JSON strings
+            self.encoder = tf.keras.models.model_from_json(
+                self.encoder_architecture_json,
+                custom_objects={
+                    "SimCLRNTXentLoss": SimCLRNTXentLoss,
+                    "GraphEmbeddings": GraphEmbeddings,
+                },
+            )
+            self.projection_head = tf.keras.models.model_from_json(
+                self.projection_head_architecture_json,
+                custom_objects={
+                    "SimCLRNTXentLoss": SimCLRNTXentLoss,
+                    "GraphEmbeddings": GraphEmbeddings,
+                },
+            )
+
+        # Create downstream layers
+        self.f_R = tf.keras.Sequential(
+            [
+                layer
+                for units in self.downstream_units
+                for layer in [
+                    tf.keras.layers.Dense(units),
+                    tf.keras.layers.BatchNormalization(),
+                    tf.keras.layers.ReLU(),
+                ]
+            ]
+        )
+
+        self.output_layer = tf.keras.layers.Dense(self.output_dim)
+
+    def set_encoder_trainable(self, trainable):
+        """
+        Set whether the encoder weights are trainable or frozen. Propagates through all layers of encoder.
+
+        Args:
+            trainable (bool): Mode
+        """
+        self.trainable_encoder = trainable
+        self.encoder.trainable = trainable
+        for layer in self.encoder.layers:
+            layer.trainable = trainable
+
+        self.trainable_projection_head = trainable
+        self.projection_head.trainable = trainable
+        for layer in self.projection_head.layers:
+            layer.trainable = trainable
+
+    def build(self, input_shape):
+        if not hasattr(self, "encoder"):
+            raise AttributeError("Encoder not loaded properly")
+        if not hasattr(self, "projection_head"):
+            raise AttributeError("Projection head not loaded properly")
+
+        dummy_input = tf.keras.layers.Input(shape=input_shape[1:])
+        embeddings = self.encoder(dummy_input)
+        projections = self.projection_head(embeddings)
+
+        self.f_R.build(projections.shape)
+
+        self.output_layer.build(tf.TensorShape([None, self.downstream_units[-1]]))
+
+        super().build(input_shape)
+
+    def call(self, inputs, training=None):
+        embeddings = self.encoder(inputs, training=training)
+
+        projections = self.projection_head(embeddings, training=training)
+
+        x = self.f_R(projections, training=training)
+
+        outputs = self.output_layer(x, training=training)
+
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.output_dim)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "model_path": self.model_path,
+                "downstream_units": self.downstream_units,
+                "encoder_architecture_json": self.encoder_architecture_json,
+                "projection_head_architecture_json": self.projection_head_architecture_json,
+                "output_dim": self.output_dim,
+                "trainable_encoder": self.trainable_encoder,
+                "trainable_projection_head": self.trainable_projection_head,
+            }
+        )
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        config_copy = deepcopy(config)
+        config_copy["load_from_path"] = False
+        return cls(**config_copy)
+
+
 class FinetuningCallback(tf.keras.callbacks.Callback):
     def __init__(
         self, freeze_epoch, low_lr, normal_lr, lr_decay_factor=0.95, verbose=1
